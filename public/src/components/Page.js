ec15336dd7f9b470c2ff6c5ec1b8e2cb3093cac4
Krproxy <krproxy@gmail.com>
2016-12-18 11:50:39 +0200

    add news

--- public/src/components/Page.js
+++ public/src/components/Page.js
@@ -8,50 +8,53 @@ export default class Page extends Component {
 	}
 	render() {
 		const { points, showMode } = this.props;
-		return <div>
-			<div data-spy='affix' data-offset-top='60'>
-			<button 
-				type='button' 
-				className={showMode.indexOf(ALL) != -1 ? 'btn btn-news btn-all active' : 'btn btn-news btn-all'}
-				onClick={::this.onYearBtnClick}>
-				<span className='transparent'>{ALL}</span>
-			</button>
-			<button
-				type='button' 
-				className={showMode.indexOf(CHECK_INN) != -1 ? 'btn btn-news btn-checkInn active' : 'btn btn-news btn-checkInn'} 
-				onClick={::this.onYearBtnClick}>
-				<span className='transparent'>{CHECK_INN}</span>
-			</button>
-			<button 
-				type='button' 
-				className={showMode.indexOf(TEA_SERVICE) != -1 ? 'btn btn-news btn-tea active' : 'btn btn-news btn-tea'}
-				onClick={::this.onYearBtnClick}>
-				<span className='transparent'>{TEA_SERVICE}</span>
-			</button>
-			<button
-				type='button'
-				className={showMode.indexOf(COUCH_SERVICE) != -1 ? 'btn btn-news btn-couch active' : 'btn btn-news btn-couch'}
-				onClick={::this.onYearBtnClick}>
-				<span className='transparent'>{COUCH_SERVICE}</span>
-			</button>
-			<button
-				type='button'
-				className={showMode.indexOf(WALK_SERVICE) != -1 ? 'btn btn-news btn-walk active' : 'btn btn-news btn-walk'}
-				onClick={::this.onYearBtnClick}>
-				<span className='transparent'>{WALK_SERVICE}</span>
-			</button>
+		return <div className='row'>
+			<div className='col-xs-3 col-sm-2 col-md-1'>
+				<div data-spy='affix' data-offset-top='60'>
+					<button 
+						type='button' 
+						className={showMode.indexOf(ALL) != -1 ? 'btn btn-news btn-allNews active' : 'btn btn-news btn-allNews'}
+						onClick={::this.onYearBtnClick}>
+						<span className='transparent'>{ALL}</span>
+					</button><br/>
+					<button
+						type='button' 
+						className={showMode.indexOf(CHECK_INN) != -1 ? 'btn btn-news btn-checkInn active' : 'btn btn-news btn-checkInn'} 
+						onClick={::this.onYearBtnClick}>
+						<span className='transparent'>{CHECK_INN}</span>
+					</button><br/>
+					<button 
+						type='button' 
+						className={showMode.indexOf(TEA_SERVICE) != -1 ? 'btn btn-news btn-tea active' : 'btn btn-news btn-tea'}
+						onClick={::this.onYearBtnClick}>
+						<span className='transparent'>{TEA_SERVICE}</span>
+					</button><br/>
+					<button
+						type='button'
+						className={showMode.indexOf(COUCH_SERVICE) != -1 ? 'btn btn-news btn-couch active' : 'btn btn-news btn-couch'}
+						onClick={::this.onYearBtnClick}>
+						<span className='transparent'>{COUCH_SERVICE}</span>
+					</button><br/>
+					<button
+						type='button'
+						className={showMode.indexOf(WALK_SERVICE) != -1 ? 'btn btn-news btn-walk active' : 'btn btn-news btn-walk'}
+						onClick={::this.onYearBtnClick}>
+						<span className='transparent'>{WALK_SERVICE}</span>
+					</button>
+				</div>
 			</div>
-
-			{points.length == 0 ? 
-				<div className='text-center'>
-					<h3>Загружаем события</h3>
-					<div className='loader'></div>
-				</div> :
-				points.map(point => {
-					return showMode.indexOf(ALL) != -1 ? <Point key={point.id} point={point} /> :
-						showMode.indexOf(point.type) != -1 ? <Point key={point.id} point={point} /> :
-							'';
-				})}
+			<div className ='col-xs-9 col-sm-10 col-md-11'>
+				{points.length == 0 ? 
+					<div className='text-center'>
+						<h3>Загружаем события</h3>
+						<div className='loader'></div>
+					</div> :
+					points.map(point => {
+						return showMode.indexOf(ALL) != -1 ? <Point key={point.id} point={point} /> :
+							showMode.indexOf(point.type) != -1 ? <Point key={point.id} point={point} /> :
+								'';
+					})}
+			</div>
 		</div>;
 	}
 }
--- public/src/components/Point.js
+++ public/src/components/Point.js
@@ -29,16 +29,19 @@ export default class Page extends Component {
 		return <div className ='row'>
 			<div className='col-xs-12'>
 				<div className='media'>
-					<div className='media-left'>
-						<h3><a className={point_class} href={ '/service/' + point.id } >
-								<img className='img37' src={point_img_src} />
-							</a></h3>
-					</div>
-					<div className='media-body media-middle'>
-						<h3><a className={point_class} href={ '/service/' + point.id }>
-							{point.address}</a></h3>
-					</div>
-				</div>
+					<a className={point_class} href={ '/service/' + point.id } >
+						<div className='media-left'>
+							<img className='img37' src={point_img_src} />							
+						</div>
+						<div className='media-body media-middle'>
+							<a className={point_class} href={ '/service/' + point.id }>
+								{point.address}</a>
+						</div>
+					</a>
+				</div>				
+			</div>
+			<div className='col-xs-12'>
+				<span className={point_class}>{point.created_at.split(' ').shift()}</span>
 			</div>
 			<div className='col-xs-12'>
 				{point.images.map(image => <img
@@ -60,10 +63,6 @@ export default class Page extends Component {
 			<div className='media-body'>
 				<span className={'f600-16 pm-0 ' + point_class}>{point.authorName}</span>
 			</div>
-			<div className='media-body media-right'>
-							<span className={'f600-16 pm-0 ' + point_class}>{point.created_at}</span>
-
-			</div>
         </a>
 
     </div>
@@ -81,46 +80,3 @@ export default class Page extends Component {
 		</div>;
 	}
 }
-
-
-// @foreach($yogaPoints as $point)
-//     @if($point->type=='teaService')
-//         <?php $point_class = "color-tea";$point_img_src = "/img/SVG/button_tea_135x135.svg"; ?>
-//     @elseif($point->type=='checkInn')
-//         <?php $point_class = "corporateBlue";$point_img_src = "/img/SVG/map_check-in_47x60.svg"; ?>
-//     @elseif($point->type=='couchService')
-//         <?php $point_class = "color-sleep";$point_img_src = "/img/SVG/button_couch_203_135.svg"; ?>
-//     @elseif($point->type=='walkServices')
-//         <?php $point_class = "color-walk";$point_img_src = "/img/SVG/button_walk_172x135 (1).svg"; ?>
-//     @endif
-
-//     <div class="media">
-//         <div class="media-left">
-//             <h3><a class="{{$point_class}}" href="/service/{{$point->id}}">
-//                     <img class="img37" src="{{$point_img_src}}"
-//                          alt="">
-//                 </a>
-//             </h3>
-//         </div>
-//         <div class="media-body media-middle">
-//             <h3><a class="{{$point_class}}" href="/service/{{$point->id}}">{{$point->address}}</a>
-//             </h3>
-//         </div>
-//     </div>
-//     {{$point->description}}
-//     <div class="media">
-//         <div class="media-left">
-//             <a href="{{URL::to('/profile/' . $point->user_id)}}">
-//                 <img src="{{isset(\app\User::findOrNew($point->user_id)->avatar)?\app\User::findOrNew($point->user_id)->avatar:asset('img/SVG/profile_12x13.svg')}}"
-//                      alt="" class="img37 circle .mt--5 media-object">
-//             </a>
-//         </div>
-//         <div class="media-body">
-//             <h4 class="media-heading">Автор:</h4>
-//             <a href="{{URL::to('/profile/' . $point->user_id)}}">
-//                 <span class="f600-16 {{$point_class}} pm-0">{{\app\User::findOrNew($point->user_id)->name}}</span>
-//             </a>
-//         </div>
-//     </div>
-//     <hr>
-// @endforeach
